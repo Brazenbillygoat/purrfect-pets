39c6f1f8778356844a113a61dd4200f0bc5f8643
Brazenbillygoat <hyrum.butler3@gmail.com>
2020-09-15 17:58:01 -0400

    Added urls to the database

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -3,7 +3,7 @@ require 'date'
 class Pet < ApplicationRecord
 
     def age
-        Time.now.year - self.created_at.year
+        Time.now.year - self.year_of_birth
     end
 
     def good_or_bad
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,8 @@
 Rails.application.routes.draw do
-  # get 'pets/index'
-  # get 'pets/show'
-  # get 'static/welcome'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
   get "/", to: "static#welcome"
   get "/pets", to: "pets#index"
   get "/pets/:id", to: "pets#show", as: "pet"
   get "/dogs", to: "pets#dog"
   get "/cats", to: "pets#cat"
   get "/birds", to: "pets#bird"
-
-
 end
